bb8a65f49e844ca59657906260638066bd4b582f
M603-droid <mridulanamdeo@gmail.com>
2021-10-04 22:46:04 +0200

    not working yet

--- src/components/PageDetails.jsx
+++ src/components/PageDetails.jsx
@@ -1,17 +1,52 @@
 import React from "react";
+import { Component } from "react";
 import { Row , Col, Card, Button } from "react-bootstrap";
-export default function MyPageDetails() {  
-
+class Home extends Component {
+  state = {
+    artworks: [], //arts will come here
+    isLoading: true,
+  };
   
+
+  componentDidMount() {
+    this.fetchArtworks();
+  }
+
+
+  fetchArtworks  = async () => {
+    try {
+      const response = await fetch(
+        `https://api.artic.edu/api/v1/artworks`   
+        
+        
+      );
+      if (response.ok) {
+        const artworks = await response.json();
+        this.setState({ artworks: artworks } ,() =>
+        console.log(this.state.artworks.data)
+        )
+      } 
+    
+      else {
+        alert("Some error happened. No movies fetched");
+        this.setState({ isLoading: false });
+      }
+    } catch (error) {
+      console.log(error);
+      this.setState({ isLoading: false });
+    }
+  };
+
+   render() {
     return (
        <Row><Col sm-4>
           <Card >
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src={this.props.image_id} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>{this.props.title}</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
+            {this.props.alt_text}
+          
           </Card.Text>
          
         </Card.Body>
@@ -21,3 +56,5 @@ export default function MyPageDetails() {
       </Row>
     )
 }
+}
+export default Home;
